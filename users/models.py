a43cde3f4702a6516b8a9f8e6cf60b4dab486151
Mara <Zainullin.marat93@yandex.ru>
2025-03-02 21:12:48 +0300

    hw8(30.2(3)

--- users/models.py
+++ users/models.py
@@ -1,6 +1,10 @@
+from datetime import datetime
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+
 from lms.models import Course, Lesson
+
 
 class User(AbstractUser):
     username = None
