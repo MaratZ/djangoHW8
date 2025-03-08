b773812f103ec4501c13d3fa7a0bddf67afa86d8
Mara <Zainullin.marat93@yandex.ru>
2025-03-08 21:23:56 +0300

    hw831

--- config/settings.py
+++ config/settings.py
@@ -64,8 +64,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
+"DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.IsAuthenticated"],
 }
-
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny
 from users.models import Payment, User
 from users.serializers import PaymentSerializer, UserSerializer
 
@@ -22,7 +23,7 @@ class PaymentViewSet(ModelViewSet):
 class UserCreateAPIView(CreateAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
-
+    permission_classes = (AllowAny,)
     def perform_create(self, serializer):
         user = serializer.save(is_active=True)
         user.set_password(user.password)
