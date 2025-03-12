7408e855b29dc794960233ff9e881934d22ec298
Mara <Zainullin.marat93@yandex.ru>
2025-03-12 23:21:54 +0300

    hw32

--- lms/serializers.py
+++ lms/serializers.py
@@ -6,13 +6,17 @@ from lms.validators import LinkValidator
 class CourseSerializer(ModelSerializer):
     lessons_quantity = SerializerMethodField()
     lessons_info = SerializerMethodField()
-
+    user_signed = SerializerMethodField(read_only=True)
     def get_lessons_quantity(self, obj):
         return obj.lesson_set.count()
 
     def get_lessons_info(self, obj):
         lessons = obj.lesson_set.all()
         return LessonSerializer(lessons, many=True).data
+
+    def get_user_signed(self, instance):
+        user = self.context["request"].user
+        return Subscription.objects.filter(user=user, course=instance).exists()
 
     class Meta:
         model = Course
