3490c536e3de20798ccaab886078099efe86fec2
Mara <Zainullin.marat93@yandex.ru>
2025-03-12 23:18:38 +0300

    hw832

--- lms/models.py
+++ lms/models.py
@@ -23,7 +23,12 @@ class Course(models.Model):
         null=True,
         help_text="Загрузите превью",
     )
-
+    user = models.ForeignKey(
+         AUTH_USER_MODEL,
+         on_delete=models.CASCADE,
+         null=True,
+         blank=True,
+     )
     class Meta:
         verbose_name = "Курс"
         verbose_name_plural = "Курсы"
@@ -65,7 +70,12 @@ class Lesson(models.Model):
         verbose_name="Ссылка на видео",
         help_text="Загрузите видео",
     )
-
+    user = models.ForeignKey(
+         AUTH_USER_MODEL,
+         on_delete=models.CASCADE,
+         null=True,
+         blank=True,
+    )
     class Meta:
         verbose_name = "Урок"
         verbose_name_plural = "Уроки"
@@ -77,9 +87,10 @@ class Lesson(models.Model):
 class Subscription(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name="Курс")
     user = models.ForeignKey(
-        AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="Пользователь"
-    )
-
+        AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        verbose_name="Пользователь",
+        )
     class Meta:
         verbose_name = "Подписка"
         verbose_name_plural = "Подписки"
