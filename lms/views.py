12a77b1e3475c0242b17a943719f6a48d0c5c943
Mara <Zainullin.marat93@yandex.ru>
2025-03-13 20:09:51 +0300

    hw832

--- lms/views.py
+++ lms/views.py
@@ -59,29 +59,30 @@ class LessonDestroyApiView(DestroyAPIView):
     serializer_class = LessonSerializer
     permission_classes = [IsAuthenticated, ~IsModer | IsOwner]
 
-    class SubscriptionCreateAPIView(CreateAPIView):
+class SubscriptionCreateAPIView(CreateAPIView):
 
-        queryset = Subscription.objects.all()
-        serializer_class = SubscriptionSerializer
+    queryset = Subscription.objects.all()
+    serializer_class = SubscriptionSerializer
 
-        def post(self, request, *args, **kwargs):
+    def post(self, request, *args, **kwargs):
 
-            user = self.request.user
-            # id курса, которое передал пользователь
-            course_id = self.request.data.get("course")
-            # сущность курса, все данные по курсу, который запросил пользователь
-            course_item = get_object_or_404(Course, pk=course_id)
-            # queryset на сущность подписки фильтр по вошедшему пользователю и курсу
-            subs_item = Subscription.objects.filter(course=course_item, user=user)
+        user = self.request.user
+        # id курса, которое передал пользователь
+        course_id = self.request.data.get("course")
+        # сущность курса, все данные по курсу, который запросил пользователь
+        course_item = get_object_or_404(Course, pk=course_id)
+        # queryset на сущность подписки фильтр по вошедшему пользователю и курсу
+        subs_item = Subscription.objects.filter(course=course_item, user=user)
 
-            if subs_item.exists():  # если такая подписка существует то удаляем
-                subs_item.delete()
-                message = "Подписка удалена"
-            else:  # иначе создаем
-                Subscription.objects.create(course=course_item, user=user)
-                message = "Подписка создана"
+        if subs_item.exists():  #  если такая подписка существует то удаляем
+            subs_item.delete()
+            message = "Подписка удалена"
+        else:  #  иначе создаем
+            Subscription.objects.create(course=course_item, user=user)
+            message = "Подписка создана"
 
-            return Response({"message": message})
+        return Response({"message": message})
+
 
 class SubscriptionListAPIView(ListAPIView):
         queryset = Subscription.objects.all()
